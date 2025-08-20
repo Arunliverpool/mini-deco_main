820eda22300fc5901d0c8c4c942a3c20e609f394
Arunkumar <arunkumar@Arunkumars-MacBook-Air.local>
2025-08-20 12:40:15 +1000

    Replace TS Next config with JS for Vercel

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // typescript: { ignoreBuildErrors: true },
+  // eslint: { ignoreDuringBuilds: true },
+  reactStrictMode: true,
+};
+module.exports = nextConfig;
